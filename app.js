9baaf6ebc70f5ceabfbfd921132c73439d7d510d
cypher <cypherskull@gmail.com>
2018-07-24 01:01:23 +0530

    Address search
    Address search app using Google API in node JS - first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+
+const yargs = require('yargs');
+
+
+
+const geocode = require('./geocode/geocode');
+
+const argv = yargs
+	
+	.options({
+	a: {
+		demand: true,
+		alias : 'address',
+		describe: 'Address to fetch weather for',
+		string: true
+	}
+})	
+.help()
+.alias('help','h')
+.argv;
+
+//console.log(argv);
+
+geocode.geocodeAddress(argv.a, (errorMsg, results) => {
+	
+	if(errorMsg) {
+		console.log(errorMsg);
+	} else {
+		console.log(JSON.stringify(results, undefined, 10));
+	} 
+});
+
+	
---
+++ geocode/geocode.js
@@ -0,0 +1,29 @@
+
+const request = require('request');
+
+var geocodeAddress = (address, callback) => {
+
+
+
+request({
+	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodeURI(address),
+	json: true
+}, (error, response, body) => {
+
+	if (error) {
+		callback('Unable to connect to google servers.')
+	} else if (body.status === 'ZERO_RESULTS') {
+		callback('Unable to find that address.');
+	} else {
+
+		callback(undefined, {
+			address: body.results[0].formatted_address,
+			latitude: body.results[0].geometry.location.lat,
+			longitude: body.results[0].geometry.location.lng,
+
+		});
+	}
+});
+};
+
+module.exports.geocodeAddress = geocodeAddress;
